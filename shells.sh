8f0829cbff298ffa0bceb21bc54900a2a3c34b21
saurabh-sigmoid <91471207+saurabh-sigmoid@users.noreply.github.com>
2021-12-21 19:24:44 +0530

    Update shells.sh

--- shells.sh
+++ shells.sh
@@ -1,56 +1,48 @@
 
 #!/bin/bash
-file=sig.conf
-component=$1
-scale=$2
-view=$3
-count=$4
-if [ -z "$view" ] || [ -z "$scale" ] || [ -z "$component" ] || [ -z "$count" ] 
-then 
-    echo 'Please enter the 4 required values!'
-    echo '(INGESTOR,JOINER,WRANGLER,VALIDATOR)'
-    echo '(HIGH,MID,LOW)'
-    echo '(Auction,Bid)'
-    echo '(0-9)'
-    exit 0 
-fi
-while ! [[ "$view" =~ Auction$|Bid$ ]]; do
-echo "The value of view has to be one of these"
-echo "  Auction"
-echo "  Bid"
-read -p "please enter a valid value  " view
-done
-while ! [[ "$scale" =~ MID$|HIGH$|LOW$ ]]; do
-echo "The value of scale has to be one of these"
-echo "  -> MID"
-echo "  -> HIGH"
-echo "  -> LOW"
-read -p "please enter a valid value  " scale
-done
-while ! [[ "$component" =~ INGESTOR$|JOINER$|WRANGLER$|VALIDATOR$ ]]; do
-echo "The value of view has to be one of these"
-echo "  -> INGESTOR"
-echo "  -> JOINER"
-echo "  -> WRANGLER"
-echo "  -> VALIDATOR"
-read -p "please enter a valid value  " component
-done
-while ! [[ "$count" =~ [0-9]{1}$ ]]; do
-echo "please enter a single digit number"
-read -p "please enter a valid value  " count
-done
-if [[ $view == "Bid" ]]; then
-view="vdopiasample-bid"
-else 
-view="vdopiasample"
-fi
-while read line; do
-IFS=';' read current_view current_scale current_component_name ETL var5 <<< $line
-IFS='=' read x current_count <<< $var5
-if [ "$view" == "$current_view" ] && [ "$component" == "$current_component_name" ]
+
+comp_read=$1
+view_read=$2
+scale_read=$3
+COUNT=$4
+filename=$5
+
+funcBid () {
+        LineNumber=$( cat $filename | grep -i -n  "$comp_read" | grep -i -n -v "$VAR" | awk -F ":" '{print $2}' )
+    conf=$( cat $filename | grep -i  "$comp_read" | grep -i -v "$VAR"  )
+    SCALE=$( echo $conf | awk -F ';' '{print $2}')
+    SELECT_COUNT=$( echo $conf | awk -F '=' '{print $2}')
+    new_conf=$( echo $conf | sed  "s/$SCALE/$scale_read/g"  )
+
+    new_conf2=$( echo $new_conf | sed  "s/$SELECT_COUNT/$COUNT/g"  )
+    sed -i "s/$conf/$new_conf2/g" $filename
+}
+
+funcAuction () {
+         LineNumber=$( cat $filename | grep -i -n "$comp_read" | grep -i -n "$VAR" | awk -F ":" '{print $2}' ) # conf
+         conf=$( cat $filename | grep -i  "$comp_read" | grep -i  "$VAR"  )
+         SCALE=$( echo $conf | awk -F ';' '{print $2}')
+         SELECT_COUNT=$( echo $conf | awk -F '=' '{print $2}')
+         new_conf=$( echo $conf | sed  "s/$SCALE/$scale_read/g"  )
+
+         new_conf2=$( echo $new_conf | sed  "s/$SELECT_COUNT/$COUNT/g"  )
+         sed -i "s/$conf/$new_conf2/g" $filename
+	 echo "has been updated $LineNumber : $SELECT_COUNT"
+}
+
+
+VAR="bid"
+if [[ "$view_read" =~ "Auction" ]]
 then
-    break
+	funcAuction	
+elif [[ ! "Auction" == "true" ]]
+then
+	echo "please type valid entry in auction"
+else
+	funcBid
+elif [[ ! $VAR == "true" ]]
+then
+	echo "Please type valid entry in bid"	
 fi
-done < $file
-new_line=$view";"$scale";"$component";"$ETL";"$x"="$count
-$(sed -i "0,/$line/{s/$line/$new_line/}" $file)
+cat $filename
+
